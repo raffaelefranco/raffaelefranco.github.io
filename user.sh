1cc10bc708f9ee35ad064dd9335c53b91aa4acd8
Raffaele Franco <52498271+raffaelefranco@users.noreply.github.com>
2021-12-24 11:17:25 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-apt install xserver-xorg xserver-xorg-core xfonts-base xinit x11-xserver-utils
-apt install xfwm4 xfce4-panel xfce4-settings xfce4-power-manager xfce4-notifyd xfce4-session xfce4-screenshooter xfce4-terminal xfdesktop4 xfce4-taskmanager xfce4-notes xfce4-notes-plugin xfce4-pulseaudio-plugin xfce4-battery-plugin xfce4-clipman xfce4-clipman-plugin
-apt install lightdm lightdm-gtk-greeter-settings blueman bluez
-apt install thunar gvfs gvfs-backends policykit-1 udisks2 policykit-1-gnome ntfs-3g
+apt install blueman bluez bluez-utils thunar gvfs gvfs-backends policykit-1 udisks2 policykit-1-gnome ntfs-3g
 apt install gstreamer1.0-plugins-good gstreamer1.0-plugins-bad pulseaudio gstreamer1.0-pulseaudio pavucontrol alsamixergui
-apt install firefox-esr thunderbird greybird-gtk-theme synaptic xarchiver featherpad geany vlc chromium filezilla transmission openssh-server cheese x11vnc wicd numix-icon-theme-circle libreoffice gimp qpdfview thunar-archive-plugin galculator showfoto nomacs kazam gcc python3-pip python-pip nmap default-jdk wget mariadb-server apache2 unzip
+apt install firefox-esr thunderbird greybird-gtk-theme synaptic xarchiver featherpad geany vlc chromium filezilla transmission openssh-server cheese x11vnc numix-icon-theme-circle libreoffice gimp qpdfview thunar-archive-plugin galculator showfoto kazam gcc python3-pip nmap default-jdk wget mariadb-server apache2 unzip
 # For SSD
 #fstrim -v /
 # For battery laptop
